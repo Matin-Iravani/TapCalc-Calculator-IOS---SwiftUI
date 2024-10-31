c6585ad149b99c727eeb8cb958793d14464a3a08
Matin-Iravani <mat.iravani@gmail.com>
2024-10-30 21:38:18 -0600

    new files

---
+++ TapCalc/TapCalc/TapCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCalcApp.swift
+//  TapCalc
+//
+//  Created by Matin Iravani on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct TapCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
